f5add56abdcd71497b1099601e460b882e80f114
Cristian Ruiz <cristian.ruiz1@upr.edu>
2018-02-21 11:02:19 -0400

    Revert "Added permiter feature" Fixing mistakes This reverts commit 086d0ae0e3376546aca8321501083c550f88d5a2.

--- src/calcAreaOfCircle.java
+++ src/calcAreaOfCircle.java
@@ -4,11 +4,10 @@ public class calcAreaOfCircle {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter the radius of the circle you wish to calculates its area: ");
+		System.out.println("Enter the radius of the circle you wish to calculates its area: ");
 		double radius = input.nextDouble();
 		double area = Math.pow(radius, 2) * Math.PI;
 		System.out.println("Area of the circle: " + area);
-		System.out.println("Half the area is: " + area/2);
 	}
 
 }
