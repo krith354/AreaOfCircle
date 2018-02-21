086d0ae0e3376546aca8321501083c550f88d5a2
Cristian Ruiz <cristian.ruiz1@upr.edu>
2018-02-21 10:41:09 -0400

    Added permiter feature

--- src/calcAreaOfCircle.java
+++ src/calcAreaOfCircle.java
@@ -4,10 +4,11 @@ public class calcAreaOfCircle {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter the radius of the circle you wish to calculates its area: ");
+		System.out.print("Enter the radius of the circle you wish to calculates its area: ");
 		double radius = input.nextDouble();
 		double area = Math.pow(radius, 2) * Math.PI;
 		System.out.println("Area of the circle: " + area);
+		System.out.println("Half the area is: " + area/2);
 	}
 
 }
